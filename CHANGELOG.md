6370a74c7b04d857363ee39e1a9b196363b318f4
nodahikaru <me@nodahikaru.com>
2021-07-23 23:10:23 +0900

    updated

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+BottomSheet adheres to [Semantic Versioning](http://semver.org/).
+
+## [Master](https://github.com/weitieda/BottomSheet)
+### Added
+
+### Changed
+
+- 1.0.6
+
+Removing List padding on examples.
+
+### Removed
---
+++ iOS Example/Sources/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  iOS Example
+//
+//  Created by Tieda Wei on 2020-04-26.
+//  Copyright © 2020 Tieda Wei. All rights reserved.
+//
+
+import SwiftUI
+import BottomSheet
+
+struct ContentView: View {
+    @State var showList = false
+    @State var showMapSetting = true
+    
+    var body: some View {
+        NavigationView {
+            List(0..<20) {
+                Text("\($0)")
+            }.listStyle(PlainListStyle())
+            .bottomSheet(isPresented: $showList, height: 500) {
+                List(20..<40) { Text("\($0)") }.listStyle(PlainListStyle())
+            }
+            .bottomSheet(
+                isPresented: $showMapSetting,
+                height: 370,
+                topBarHeight: 16,
+                topBarCornerRadius: 16,
+                showTopIndicator: false
+            ) {
+                MapSettingView()
+            }
+            .navigationBarTitle("Bottom Sheet")
+            .navigationBarItems(
+                leading: Button(action: { self.showMapSetting = true }) { Text("Setting") },
+                trailing: Button(action: { self.showList = true }) { Text("List") }
+            )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+            
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+                .environment(\.colorScheme, .dark)
+        }
+    }
+}
